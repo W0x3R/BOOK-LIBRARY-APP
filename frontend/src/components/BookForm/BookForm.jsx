20ac8fda5d941b752f659925f9589cd39bedf533
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-26 18:54:46 +0300

    feat: added booklist,bookform and filter components (Sat, Oct 26, 2024, 6:54:30 PM)

---
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -0,0 +1,7 @@
+export const BookForm = () => {
+	return (
+		<div className="app-block book-form">
+			<h2>Add a New Book</h2>
+		</div>
+	)
+}
---
+++ frontend/src/components/Filter/Filter.jsx
@@ -0,0 +1,7 @@
+export const Filter = () => {
+	return (
+		<div className="app-block filter">
+			<h2>Filters</h2>
+		</div>
+	)
+}
