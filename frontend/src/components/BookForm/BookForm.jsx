870dd4b30ff2e65bbae8aa8bf4ee7096de4012c6
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-27 16:16:44 +0300

    feat: added book id (Sun, Oct 27, 2024, 4:16:38 PM)

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
+import { v4 as uuidv4 } from "uuid"
 import "./BookForm.css"
 import { addBook } from "../redux/books/actionCreators"
 
@@ -14,7 +15,8 @@ export const BookForm = () => {
 		if (title && author) {
 			const book = {
 				title,
-				author
+				author,
+				id: uuidv4()
 			}
 			dispatch(addBook(book))
 			setTitle("")
