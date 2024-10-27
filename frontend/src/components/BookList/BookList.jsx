f1ec30cd4958f083ea86fe2771cb77110695e4fd
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-27 16:10:41 +0300

    feat: the booklist component implements subscription to redux store when books state changes. added rendering of books in the application. (Sun, Oct 27, 2024, 4:10:34 PM)

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,7 +1,26 @@
+import { useSelector } from "react-redux"
+import "./BookList.css"
+
 export const BookList = () => {
+	const books = useSelector((state) => state.books)
 	return (
 		<div className="app-block book-list">
 			<h2>BookList</h2>
+			{books.length === 0 ? (
+				<p>No books available</p>
+			) : (
+				<ul>
+					{books.map((book, i) => {
+						return (
+							<li key={i}>
+								<div className="book-info">
+									{++i}. {book.title} by <strong>{book.author}</strong>
+								</div>
+							</li>
+						)
+					})}
+				</ul>
+			)}
 		</div>
 	)
 }
