5c6978e498befe344be13732b783d686df7ada58
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-27 15:53:58 +0300

    feat: created functionality to send a new book to redux store (Sun, Oct 27, 2024, 3:53:52 PM)

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react"
+import { useDispatch } from "react-redux"
 import "./BookForm.css"
+import { addBook } from "../redux/books/actionCreators"
 
 export const BookForm = () => {
 	const [title, setTitle] = useState("")
 	const [author, setAuthor] = useState("")
+	const dispatch = useDispatch()
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 
 		if (title && author) {
-			console.log(title, author)
+			const book = {
+				title,
+				author
+			}
+			dispatch(addBook(book))
 			setTitle("")
 			setAuthor("")
 		}
