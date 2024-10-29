a4cf0a311f80f03aebb0c8b622b7a15598d0f7d9
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-29 18:48:04 +0300

    feat: added functionality for clearing filters when clicking on the button (Tue, Oct 29, 2024, 6:47:59 PM)

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from "react-redux"
-import { selectTitleFilter, setTitleFilter } from "../redux/slices/filterSlice"
+import {
+	resetFilters,
+	selectTitleFilter,
+	setTitleFilter
+} from "../redux/slices/filterSlice"
 import "./Filter.css"
 
 export const Filter = () => {
@@ -9,15 +13,24 @@ export const Filter = () => {
 	const handleTitleFilterChange = (e) => {
 		dispatch(setTitleFilter(e.target.value))
 	}
+
+	const handleResetFilters = () => {
+		dispatch(resetFilters())
+	}
 	return (
 		<div className="app-block filter">
-			<div className="filter-group">
-				<input
-					value={titleFilter}
-					onChange={handleTitleFilterChange}
-					type="text"
-					placeholder="Filter by title..."
-				></input>
+			<div className="filter-row">
+				<div className="filter-group">
+					<input
+						value={titleFilter}
+						onChange={handleTitleFilterChange}
+						type="text"
+						placeholder="Filter by title..."
+					></input>
+				</div>
+				<button type="button" onClick={handleResetFilters}>
+					Reset filters
+				</button>
 			</div>
 		</div>
 	)
--- frontend/src/components/redux/slices/filterSlice.js
+++ frontend/src/components/redux/slices/filterSlice.js
@@ -10,11 +10,14 @@ const filterSlice = createSlice({
 	reducers: {
 		setTitleFilter: (state, action) => {
 			state.title = action.payload
+		},
+		resetFilters: (state) => {
+			return initialState
 		}
 	}
 })
 
-export const setTitleFilter = filterSlice.actions.setTitleFilter
+export const { setTitleFilter, resetFilters } = filterSlice.actions
 export const selectTitleFilter = (state) => state.filter.title
 
 export default filterSlice.reducer
