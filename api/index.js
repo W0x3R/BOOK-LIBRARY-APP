343bb81b906ac49ae0c6e762a6d0bee648905c2b
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-30 16:08:55 +0300

    feat: created api serivce (Wed, Oct 30, 2024, 4:08:49 PM)

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const cors = require("cors")
+const booksData = require("./data/books.json")
+
+const app = express()
+
+app.use(cors())
+
+const generateRandomBookNumber = (min, max) => {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const getRandomBook = () => {
+	let randomBook = booksData[generateRandomBookNumber(0, booksData.length - 1)]
+	return randomBook
+}
+
+app.get("/random-book", (req, res) => {
+	res.json(getRandomBook())
+})
+
+app.get("/random-book-delayed", (req, res) => {
+	setTimeout(() => {
+		res.json(getRandomBook())
+	}, 2000)
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
