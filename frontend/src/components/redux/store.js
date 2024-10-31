da5f91f5de4ee2730a34a9de7df90db44d36e79e
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-10-31 15:49:33 +0300

    feat: added errorslice (Thu, Oct 31, 2024, 3:49:25 PM)

--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import filterReducer from "./slices/filterSlice"
 import bookReducer from "./slices/booksSlice"
+import errorReducer from "./slices/errorSlice"
 
 export const store = configureStore({
 	reducer: {
 		books: bookReducer,
-		filter: filterReducer
+		filter: filterReducer,
+		error: errorReducer
 	}
 })
